b6709db51ce7fdbfe9fddc6bf46fa941097a39a5
Martin House <torchester@gmail.com>
2023-02-12 20:31:33 +0500

    add task 1.3

---
+++ 001/Task3.java
@@ -0,0 +1,41 @@
+//? Реализовать простой калькулятор (+ - / *)
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+
+            System.out.println("Enter first number: ");
+            double num1 = scan.nextDouble();
+
+            System.out.println("Enter second number: ");
+            double num2 = scan.nextDouble();
+
+            System.out.println("Enter an operator (+, -, /, *): ");
+            char operator = scan.next().charAt(0);
+
+            double result;
+            
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("Error! operator is not correct");
+                    return;
+            }
+
+            System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
+        }
+    }
+}
